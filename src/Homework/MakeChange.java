1e0596be66b4a84cda9d62453266127b613e0807
amandue10 <50896687+amandue10@users.noreply.github.com>
2021-07-23 18:06:55 -0600

    working input

--- src/Homework/MakeChange.java
+++ src/Homework/MakeChange.java
@@ -1,9 +1,24 @@
 package Homework;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		
+System.out.println("Please enter the price of the item ");		
+double priceOfItem = kb.nextDouble();
+
+System.out.println("Amount tendered:  ");		
+double amountTendered = kb.nextDouble();
+
+if (amountTendered < priceOfItem) {
+	System.out.println("Alert customer amount tendered less than price of the item.");
+} else if (amountTendered == priceOfItem) {
+System.out.println("Exact amount, no change needed");
+}
+kb.close();
 
 	}
 
